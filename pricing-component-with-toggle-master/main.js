d79407aff92d23e012e953d8eafad73488a7e3eb
jorge.mora <jmora2ec@gmail.com>
2022-06-20 22:49:58 -0500

    finished desktop version

--- pricing-component-with-toggle-master/main.js
+++ pricing-component-with-toggle-master/main.js
@@ -3,12 +3,18 @@ const paymentSelector = document.querySelector('.price-toggle input');
 const monthlyTags = document.querySelectorAll('.plan__price .monthly');
 const annualTags = document.querySelectorAll('.plan__price .annual');
 
-paymentSelector.addEventListener('click', (e) => {
-  if (e.target.checked === true) {
+function checkPayment(e) {
+  if (e.checked === true) {
     monthlyTags.forEach((element) => element.classList.add('active'));
     annualTags.forEach((element) => element.classList.remove('active'));
   } else {
     monthlyTags.forEach((element) => element.classList.remove('active'));
     annualTags.forEach((element) => element.classList.add('active'));
   }
+}
+
+paymentSelector.addEventListener('click', (e) => {
+  checkPayment(e.target);
 });
+
+checkPayment(paymentSelector);
